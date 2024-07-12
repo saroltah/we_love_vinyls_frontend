c66755a4161135e249b6355599757d69f0dd2d4e
Sarolta <115449000+saroltah@users.noreply.github.com>
2024-07-12 19:03:54 +0200

    set up bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import logo from "./logo.svg";
+import Button from "react-bootstrap/Button";
 import "./App.css";
+import Alert from "react-bootstrap/Alert";
+import Nav from "react-bootstrap/Nav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">TEST</header>
+      <p>TEST</p>
+      <Alert variant="success">
+        <Alert.Heading>Hey, nice to see you</Alert.Heading>
+        <p>
+          Aww yeah, you successfully read this important alert message. This
+          example text is going to run a bit longer so that you can see how
+          spacing within an alert works with this kind of content.
+        </p>
+        <hr />
+        <p className="mb-0">
+          Whenever you need to, be sure to use margin utilities to keep things
+          nice and tidy.
+        </p>
+      </Alert>
+      <Button variant="primary">Primary</Button>
+      <Nav
+        activeKey="/home"
+        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled>
+            Disabled
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
   );
 }
