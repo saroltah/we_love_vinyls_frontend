8eb83b38c3101b91fc89fd21857c0c1d1981dc9d
Sarolta Hegyi <sarolta.hegyi@gmail.com>
2024-07-27 09:50:05 +0000

    add like and attendance

--- src/pages/ShowMarket.js
+++ src/pages/ShowMarket.js
@@ -3,23 +3,59 @@ import { useCurrentUser } from "../context/CurrentUserContext";
 import { Link } from "react-router-dom";
 import ProfilePic from "../elements/ProfilePic";
 import {OverlayTrigger, Tooltip } from "react-bootstrap";
+import { axiosRes } from "../api/AxiosDefaults";
 
 const ShowMarket = (props) => {
   const {
     id, organizer, country, city, address, date,
             start, end, description,
-            members_attending_count, oneMarket, created, profile_slug, profile_image, attendance_id
+            members_attending_count, oneMarket, created, profile_id, profile_image, attendance_id, setMarkets
   } = props;
 
   const currentUser = useCurrentUser();
   const is_organizer = currentUser?.username === organizer;
 
+  const submitAttendance = async () => {
+    try {
+      const { data } = await axiosRes.post("/attendance/", { market: id });
+      setMarkets((prevMarkets) => ({
+        ...prevMarkets,
+        results: prevMarkets.results.map((market) => {
+          return market.id === id
+            ? { ...market, members_attending_count: market.members_attending_count + 1, attendance_id: data.id }
+            : market;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const submitNotAttending = async () => {
+    try {
+      await axiosRes.delete(`/attendance/${attendance_id}/`);
+      setMarkets((prevMarkets) => ({
+        ...prevMarkets,
+        results: prevMarkets.results.map((market) => {
+          return market.id === id
+            ? { ...market, members_attending_count: market.members_attending_count - 1, attendance_id: null }
+            : market;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
-          <Link to={`/users/${profile_slug}`}>
-            <ProfilePic src={profile_image} height={55} />
-            {organizer}
-          </Link>
+           <Link to={`/users/${profile_id}`}>
+      <ProfilePic
+          src={profile_image}
+          height={50}
+        />
+        {organizer}
+        </Link>
           <div>
             <span>{created}</span>
             {is_organizer && oneMarket && "..."}
@@ -33,31 +69,31 @@ const ShowMarket = (props) => {
 <p>Where? <span> {address}, {city}, {country}</span></p>
 <p>When? <span> {date}, {start} - {end}</span></p>
 
-        <div>
-          {is_organizer ? (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>You can't change attendance for your own market!</Tooltip>}
-            >
-              <span onClick={() => {}}>😉</span>
-            </OverlayTrigger>
-          ) : attendance_id ? (
-            <span onClick={() => {}}>
-              ✨
-            </span>
-          ) : currentUser ? (
-            <span onClick={() => {}}>
-              😊
-            </span>
-          ) : (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>Log in to attend!</Tooltip>}
-            >
-              <span onClick={() => {}}>🙌</span>
-            </OverlayTrigger>
-          )}
-          {members_attending_count}
+<div>
+      {is_organizer ? (
+        <OverlayTrigger
+        placement="top"
+        overlay={<Tooltip>You can not change attendance at your own market!</Tooltip>}
+      >
+          <i>❤️</i>
+        </OverlayTrigger>
+      ) : attendance_id ? (
+        <span onClick={submitNotAttending}>
+          <i>❤️</i>
+        </span>
+      ) : currentUser ? (
+        <span onClick={submitAttendance}>
+          <i>🤍</i>
+        </span>
+      ) : (
+        <OverlayTrigger
+        placement="top"
+        overlay={<Tooltip>You have to log in to show attendance!</Tooltip>}
+      >
+          <i>🤍</i>
+        </OverlayTrigger>
+      )}
+      {members_attending_count}
         </div>
         </div>
   );
--- src/pages/ShowRecord.js
+++ src/pages/ShowRecord.js
@@ -4,37 +4,78 @@ import { Link } from "react-router-dom";
 import ProfilePic from "../elements/ProfilePic";
 import {OverlayTrigger, Tooltip, Card } from "react-bootstrap";
 import OneRecord from "./OneRecord";
+import { axiosRes } from "../api/AxiosDefaults";
 
 const ShowRecord = (props) => {
   const {
     id, advertiser, artist, title, track_list,
     created, condition, image, released, genre,
     members_liking_count, comment_count,
-    price, location, contact, profile_slug, profile_image, like_id  
+    price, location, contact, profile_id, profile_image, like_id, setRecords
   } = props;
 
   const currentUser = useCurrentUser();
   const is_advertiser = currentUser?.username === advertiser;
 
+  //innen 
+
+  const submitLike = async () => {
+    try {
+      const { data } = await axiosRes.post("/likes/", { record: id });
+      setRecords((prevRecords) => ({
+        ...prevRecords,
+        results: prevRecords.results.map((record) => {
+          return record.id === id
+            ? { ...record, members_liking_count: record.members_liking_count + 1, like_id: data.id }
+            : record;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const submitUnlike = async () => {
+    try {
+      await axiosRes.delete(`/likes/${like_id}/`);
+      setRecords((prevRecords) => ({
+        ...prevRecords,
+        results: prevRecords.results.map((record) => {
+          return record.id === id
+            ? { ...record, members_liking_count: record.members_liking_count - 1, like_id: null }
+            : record;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //idáig
+
   return (
   <div>
-          <Link to={`/users/${profile_slug}`}>
-            <ProfilePic src={profile_image} height={55} />
-            {advertiser}
-          </Link>
-          <div>
-            <span>{created}</span>
-            {is_advertiser && OneRecord && "..."}
-          </div>
+    <div>
+      <Link to={`/users/${profile_id}`}>
+      <ProfilePic
+          src={profile_image}
+          height={50}
+        />
+        {advertiser}
+        </Link>
+        <div>
+        <span>{created}</span>
+        {is_advertiser && OneRecord && "..."}
+      </div>
+    </div>
 
-      <Link to={`/records/${id}`}>
-      <Card.Img src={image} alt={title} />
-      </Link>
-      
+    <Link to={`/records/${id}`}>
+    <img src={image} alt={title} height={100}/>
+  </Link>
+      <div>
       <h3>{artist}: {title} </h3>
       <p>Artist: {artist}</p>
       <p>Title: {title}</p>
-      <p>Year: {created}</p>
       <p>Track list: {track_list}</p>
       <p>Year: {released}</p>
       <p>Genre: {genre}</p>
@@ -43,35 +84,37 @@ const ShowRecord = (props) => {
       <p>Price: {price}</p>
       <p>Contact: {contact}</p>
 
+    
         <div>
-          {is_advertiser ? (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>You can not like your own post!</Tooltip>}
-            >
-              <span>🙌</span>
-            </OverlayTrigger>
-          ) : like_id ? (
-            <span onClick={() => {}}>
-              😊
-            </span>
-          ) : currentUser ? (
-            <span onClick={() => {}}>
-              🌿
-            </span>
-          ) : (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>You need to log in to like posts!</Tooltip>}
-            >
-              <span>👈</span>
-            </OverlayTrigger>
-          )}
-          {members_liking_count}
-          <Link to={`/records/${id}`}>
-            <span>⬅️➡️</span>
-          </Link>
-          {comment_count}
+      {is_advertiser ? (
+        <OverlayTrigger
+        placement="top"
+        overlay={<Tooltip>You can not like your own posts!</Tooltip>}
+      >
+          <i>❤️</i>
+        </OverlayTrigger>
+      ) : like_id ? (
+        <span onClick={submitUnlike}>
+          <i>❤️</i>
+        </span>
+      ) : currentUser ? (
+        <span onClick={submitLike}>
+          <i>🤍</i>
+        </span>
+      ) : (
+        <OverlayTrigger
+        placement="top"
+        overlay={<Tooltip>You have to log in to like posts!</Tooltip>}
+      >
+          <i>🤍</i>
+        </OverlayTrigger>
+      )}
+      {members_liking_count}
+      <Link to={`/records/${id}`}>
+        <i>💬</i>
+      </Link>
+      {comment_count}
+        </div>
         </div>
         </div>
   );
